074c93eafc9effa5d5df4738a3b71fa88f13f462
shuimengzhi <shui_jingshan@163.com>
2020-03-20 13:05:23 +0800

    完成DOCKER环境配置

--- conf/conf.go
+++ conf/conf.go
@@ -7,13 +7,12 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/liudng/godump"
 )
 
 // Init 初始化配置项
 func Init() {
 	// 从本地读取环境变量
-	godotenv.Load()
+	godotenv.Load("/go/src/giligili/.env")
 
 	// 设置日志级别
 	util.BuildLogger(os.Getenv("LOG_LEVEL"))
@@ -22,9 +21,7 @@ func Init() {
 	if err := LoadLocales("/go/src/giligili/conf/locales/zh-cn.yaml"); err != nil {
 		util.Log().Panic("翻译文件加载失败", err)
 	}
-	a := os.Getenv("GIN_MODE")
-	godump.Dump(a)
-	os.Exit(1)
+
 	// 连接数据库
 	model.Database(os.Getenv("MYSQL_DSN"))
 	cache.Redis()
